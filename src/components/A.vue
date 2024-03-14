e42ef8849bd56dce0809987363e3777cdb5701f1
Sean <fengying1028@hotmail.com>
2024-03-13 23:19:15 -0600

    递归

--- src/components/A.vue
+++ src/components/A.vue
@@ -2,9 +2,10 @@
     <h1 class="px-36 font-medium underline">
         Hello world!
     </h1>
-    <div v-for="(item,index) in data" :key="index" class="ml-5">
+    <!-- 防止冒泡 -->
+    <div v-for="(item,index) in data" :key="index" class="ml-5" @click.stop="print(item)">
         <input type="checkbox" v-model="item.checked"><span>{{ item.name }}</span>
-        <A :data="item.children" v-if="item?.children"></A>
+        <A :data="item.children" v-if="item?.children?.length"></A>
     </div>
 </template>
 
@@ -18,6 +19,11 @@ interface Tree {
 defineProps<{
     data?: Tree[]
 }>();
+
+const print = (item) => {
+    console.log(item);
+    
+}
 </script>
 
 <style scoped></style>
