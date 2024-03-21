fb23c78082b973f26ab6348f58d2b3362c12ca63
Sean <fengying1028@hotmail.com>
2024-03-20 19:54:27 -0600

    docs: configs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+//引入element插件和样式
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "@/App.vue";
+//svg插件所需
+import "virtual:svg-icons-register";
+//引入仓库
+import pinia from "@/store";
+import globaleComponent from "@/components/index";
+import { router } from "@/router/index";
+import "@/style/index.less";
+import '@/permission'
+const app = createApp(App);
 
-import App from './App.vue';
-import './index.css';
-createApp(App).mount('#app');
+app.use(globaleComponent);
+app.use(ElementPlus);
+app.use(router);
+app.use(pinia);
+app.mount("#app");
