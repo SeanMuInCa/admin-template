a50236ad0134c40188488969b4ff49e4e2cd0de0
Sean <fengying1028@hotmail.com>
2024-03-22 19:38:59 -0600

    feat: get and set local storage

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -2,22 +2,22 @@
 
 import { defineStore } from 'pinia';
 import { loginRequest } from '@/api/user';
-import {UserState} from './types/type'
+import { UserState } from './types/type';
 import { loginData, loginReturnData } from '@/api/user/type';
-import {SET_TOKEN, GET_TOKEN} from '@/utils/token'
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token';
 const useUserStore = defineStore('User', {
-  state: ():UserState => {
+  state: (): UserState => {
     return {
-      token: GET_TOKEN()
+      token: GET_TOKEN(),
     };
   },
 
   actions: {
     async userLogin(loginInfo: loginData) {
-      const data:loginReturnData = await loginRequest(loginInfo);
+      const data: loginReturnData = await loginRequest(loginInfo);
 
       if (data.code === 200) {
-        this.token = (data.data.token as string);//断言
+        this.token = data.data.token as string; //断言
         SET_TOKEN(data.data.token);
         return Promise.resolve(data.code);
       } else {
