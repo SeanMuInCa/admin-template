ecfb0cc662ec329a55301f69523f578e564b3ec5
Sean <fengying1028@hotmail.com>
2024-03-23 23:20:10 -0600

    feat: sub menu

--- src/layout/menu.vue
+++ src/layout/menu.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="menu">
-    <el-menu v-for="(item, index) in menuList" :key="item.path">
-      <el-menu-item v-if="!item.children" :index="index + 1">{{ item.name }}</el-menu-item>
-      <el-sub-menu v-else v-for="(sub, index) in item.children" :key="index">
-        <template #title>
-          {{ item.name }}
-        </template>
-        <el-menu-item>{{ sub.name }}</el-menu-item>
-      </el-sub-menu>
-    </el-menu>
-  </div>
+    <div class="menu">
+        <el-menu v-for="(item, index) in menuList" :key="item.path">
+            <el-menu-item v-if="!item.children" :index="item.path">xxx{{ item.meta.title }}</el-menu-item>
+            <template #title>
+                xxx{{ item.meta.title }}
+            </template>
+            <el-menu-item v-if="item.children && item.children.length == 1" :index="item.children[0].path">
+                <template #title>
+                    xxx{{ item.children[0].meta.title }}
+                </template>
+            </el-menu-item>
+            <el-sub-menu v-if="item.children && item.children.length >1">
+                <template #title>
+                    xxx{{ item.meta.title }}
+                </template>
+                <el-menu-item v-for="(sub, index) in item.children" :key="index">xxx{{ sub.meta.title }}</el-menu-item>
+            </el-sub-menu>
+        </el-menu>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -19,6 +27,6 @@ defineProps(['menuList']);
 
 <style scoped lang="scss">
 .menu {
-  height: 100%;
+    height: 100%;
 }
 </style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,16 +3,25 @@ export const staticRoutes = [
     path: '/login',
     component: () => import('@/views/login/index.vue'),
     name: 'login',
+    meta:{
+      title: 'Login'
+    }
   },
   {
     path: '/',
     component: () => import('@/layout/index.vue'),
     name: 'layout',
+    meta:{
+      title:'Layout'
+    },
     children: [
       {
         path: '/home',
         component: () => import('@/views/home/index.vue'),
         name: 'home',
+        meta:{
+          title:'Home'
+        },
       },
     ],
   },
@@ -20,10 +29,16 @@ export const staticRoutes = [
     path: '/404',
     component: () => import('@/views/404/index.vue'),
     name: '404',
+    meta:{
+      title:'404'
+    },
   },
   {
     path: '/:pathMatch(.*)*',
     redirect: '/404',
     name: 'redirect404',
+    meta:{
+      title:'Redirect404'
+    },
   },
 ];
