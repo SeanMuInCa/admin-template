4f4cfb18eaed6a81df0f5b0a54e27a0935d5ed6f
Sean <fengying1028@hotmail.com>
2024-03-27 23:28:08 -0600

    feat: permission

--- src/permission.ts
+++ src/permission.ts
@@ -3,19 +3,53 @@
 import { router } from '@/router/index';
 import nprogress from 'nprogress';
 import 'nprogress/nprogress.css';
+import pinia from '@/store';
+import useUserStore from '@/store/modules/user';
+const userStore = useUserStore(pinia); //不加入pinia会报错
+
 //全局守卫
 
 //前置守卫
-router.beforeEach((to: any, from: any, next: any) => {
-  // to and from are both route objects. must call `next`.
-  nprogress.start();
-  console.log(to);
-  console.log(from);
-  next();
+router.beforeEach(async(to: any, from: any, next: any) => {
+    // to and from are both route objects. must call `next`.
+    const token = userStore.token;
+    
+    // console.log(token,username);
+    //login
+    if (token) {
+        const username = userStore.userInfo.username;
+        if (to.path === '/login') {
+            next({ path: '/home' });
+        }else{
+            // console.log(username);
+            next();
+            // if(username) {
+            //     next();
+            // }
+            // else{
+            //     userStore.requestInfo();
+            // }
+        }
+        //un login
+    } else {
+        if (to.path == '/login') {
+            next();
+        } else {
+            console.log(1);
+            
+            next({
+                path: '/login',
+                query: {
+                    redirect: to.path,
+                }
+            });
+        }
+    }
+    nprogress.start();
 });
 
 //后置守卫
 router.afterEach((to: any, from: any) => {
-  // to and from are both route objects.
-  nprogress.done();
+    // to and from are both route objects.
+    nprogress.done();
 });
