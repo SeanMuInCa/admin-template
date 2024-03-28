6164757969d1691af52b722b38a02623760e40c3
Sean <fengying1028@hotmail.com>
2024-03-27 23:28:25 -0600

    feat: permission

--- src/permission.ts
+++ src/permission.ts
@@ -10,46 +10,46 @@ const userStore = useUserStore(pinia); //不加入pinia会报错
 //全局守卫
 
 //前置守卫
-router.beforeEach(async(to: any, from: any, next: any) => {
-    // to and from are both route objects. must call `next`.
-    const token = userStore.token;
-    
-    // console.log(token,username);
-    //login
-    if (token) {
-        const username = userStore.userInfo.username;
-        if (to.path === '/login') {
-            next({ path: '/home' });
-        }else{
-            // console.log(username);
-            next();
-            // if(username) {
-            //     next();
-            // }
-            // else{
-            //     userStore.requestInfo();
-            // }
-        }
-        //un login
+router.beforeEach(async (to: any, from: any, next: any) => {
+  // to and from are both route objects. must call `next`.
+  const token = userStore.token;
+
+  // console.log(token,username);
+  //login
+  if (token) {
+    const username = userStore.userInfo.username;
+    if (to.path === '/login') {
+      next({ path: '/home' });
     } else {
-        if (to.path == '/login') {
-            next();
-        } else {
-            console.log(1);
-            
-            next({
-                path: '/login',
-                query: {
-                    redirect: to.path,
-                }
-            });
-        }
+      // console.log(username);
+      next();
+      // if(username) {
+      //     next();
+      // }
+      // else{
+      //     userStore.requestInfo();
+      // }
     }
-    nprogress.start();
+    //un login
+  } else {
+    if (to.path == '/login') {
+      next();
+    } else {
+      console.log(1);
+
+      next({
+        path: '/login',
+        query: {
+          redirect: to.path,
+        },
+      });
+    }
+  }
+  nprogress.start();
 });
 
 //后置守卫
 router.afterEach((to: any, from: any) => {
-    // to and from are both route objects.
-    nprogress.done();
+  // to and from are both route objects.
+  nprogress.done();
 });
