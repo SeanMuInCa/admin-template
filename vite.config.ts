db47a2ce34055bf1db771eec5b7cf9e904cb8dd5
Sean <fengying1028@hotmail.com>
2024-03-28 10:57:00 -0600

    feat: cors config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 //svg引入
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { UserConfigExport, ConfigEnv } from 'vite';
 import { viteMockServe } from 'vite-plugin-mock';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的env
+  const env = loadEnv(mode, process.cwd());
   return {
     plugins: [
       vue(),
@@ -32,6 +34,17 @@ export default defineConfig(({ command }) => {
         scss: {
           javascriptEnabled: true,
           additionalData: '@import "./src/styles/variables.scss";',
+        },
+      },
+    },
+    //代理跨域
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          changeOrigin: true,
+          //路径重写，这里的意思是，把/api替换为空，因为接口文档里是并没有/api的
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
