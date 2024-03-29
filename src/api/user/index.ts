f27e64bffc4b299d5beeacc20db820927223d053
Sean <fengying1028@hotmail.com>
2024-03-29 00:28:57 -0600

    feat: new api

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,17 +1,16 @@
 //统一管理项目用户相关的接口
 import axiosRequest from '@/utils/request';
 
-
 //统一管理接口
 
 enum API {
   LOGIN_URL = '/admin/acl/index/login',
   USERINFO_URL = '/admin/acl/index/info',
-  LOGOUT_URL = '/admin/acl/index/logout'
+  LOGOUT_URL = '/admin/acl/index/logout',
 }
 
 //login
-export const loginRequest = (data:any) => axiosRequest.post<any,any>(API.LOGIN_URL, data);
+export const loginRequest = (data: any) => axiosRequest.post<any, any>(API.LOGIN_URL, data);
 
 //userinfo
 export const requestInfo = () => axiosRequest.get<any>(API.USERINFO_URL);
