0140646174fd33af8f1bfa523e8f0068a7e5e3c2
Sean <fengying1028@hotmail.com>
2024-04-02 10:45:43 -0600

    feat: add attributes

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -2,17 +2,17 @@
   <el-card>
     <el-form class="demo-form-inline" :inline="true">
       <el-form-item label="Category I">
-        <el-select placeholder="please select category" clearable style="width: 240px" v-model="categoryStore.c1_id" @change="selectA" @clear="clearA">
+        <el-select placeholder="please select category" clearable style="width: 240px" v-model="categoryStore.c1_id" @change="selectA" :disabled="!showTable">
           <el-option v-for="item in categoryStore.c1Arr" :key="item.id" :label="item.name" :value="item.id" />
         </el-select>
       </el-form-item>
       <el-form-item label="Category II">
-        <el-select v-model="categoryStore.c2_id" clearable placeholder="please select category" @change="selectB" style="width: 240px">
+        <el-select v-model="categoryStore.c2_id" clearable placeholder="please select category" @change="selectB" style="width: 240px" :disabled="!showTable">
           <el-option v-for="item in categoryStore.c2Arr" :key="item.id" :label="item.name" :value="item.id" />
         </el-select>
       </el-form-item>
       <el-form-item label="Category III">
-        <el-select v-model="categoryStore.c3_id" clearable placeholder="please select category" @change="selectC" style="width: 240px">
+        <el-select v-model="categoryStore.c3_id" clearable placeholder="please select category" style="width: 240px" :disabled="!showTable">
           <el-option v-for="item in categoryStore.c3Arr" :key="item.id" :label="item.name" :value="item.id" />
         </el-select>
       </el-form-item>
@@ -25,7 +25,7 @@ import { onMounted } from 'vue';
 import useCategoryStore from '@/store/modules/category';
 
 const categoryStore = useCategoryStore();
-const props = defineProps(['setFlag']);
+const props = defineProps(['showTable']);
 
 onMounted(() => {
   getCategoryInit();
--- src/views/product/Attitude.vue
+++ src/views/product/Attitude.vue
@@ -1,36 +1,58 @@
 <template>
-  <Category></Category>
+  <Category :showTable="showTable"></Category>
   <el-card>
-    <el-button type="primary" icon="Plus" @click="handleAdd" :disabled="!categoryStore.c3_id">add an attribute</el-button>
-    <el-table :data="categoryStore.list" style="width: 100%; margin: 20px 0" height="600" border v-loading="categoryStore.loading">
-      <el-table-column fixed type="index" prop="index" label="No." width="100px" align="center" />
-      <el-table-column prop="attrName" label="Attribute Name" width="300px"></el-table-column>
-      <el-table-column prop="attrValueList" label="Attribute Value Name" width="700px" style="display: flex; justify-content: space-around">
-        <template #default="{ row }">
-          <!-- <span v-for="item in row.attrValueList" :key="item.id" style="display: inline-block; padding: 5px; margin: 0 10px; background-color: aqua " >{{ item.valueName }}</span> -->
-          <div class="tags">
-            <el-tag v-for="item in row.attrValueList" :key="item.id" :type="Math.random() > 0.5 ? 'success' : 'warning'">{{ item.valueName }}</el-tag>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column prop="" label="Operation">
-        <template #default="{ row }">
-          <el-button type="warning" icon="Edit"></el-button>
-          <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?">
-            <template #reference>
-              <el-button type="danger" icon="Delete"></el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div v-if="showTable">
+      <el-button type="primary" icon="Plus" @click="handleAdd" :disabled="!categoryStore.c3_id">add an
+        attribute</el-button>
+      <el-table :data="categoryStore.list" style="width: 100%; margin: 20px 0" height="600" border
+        v-loading="categoryStore.loading">
+        <el-table-column fixed type="index" prop="index" label="No." width="100px" align="center" />
+        <el-table-column prop="attrName" label="Attribute Name" width="300px"></el-table-column>
+        <el-table-column prop="attrValueList" label="Attribute Value Name" width="700px"
+          style="display: flex; justify-content: space-around">
+          <template #default="{ row }">
+            <div class="tags">
+              <el-tag v-for="item in row.attrValueList" :key="item.id"
+                :type="Math.random() > 0.5 ? 'success' : 'warning'">{{ item.valueName }}</el-tag>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="" label="Operation">
+          <template #default="{ row }">
+            <el-button type="warning" icon="Edit" @click="handleEdit"></el-button>
+            <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled"
+              icon-color="#626AEF" title="Are you sure to delete this?">
+              <template #reference>
+                <el-button type="danger" icon="Delete"></el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div v-else>
+      <el-form :inline="true">
+        <el-form-item label="Attribute Name">
+          <el-input placeholder="Input Attribute Name" ></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" icon="Plus">Add Attribute Value</el-button>
+      <el-button  @click="cancel">Cancel</el-button>
+      <el-table border style="margin: 10px 0;">
+        <el-table-column label="No." width="100px" align="center" type="index"></el-table-column>
+        <el-table-column label="Attribute Value"></el-table-column>
+        <el-table-column label="Operation"></el-table-column>
+      </el-table>
+      <el-button type="primary" >Confirm</el-button>
+      <el-button  @click="cancel">Cancel</el-button>
+    </div>
   </el-card>
 </template>
 
 <script setup lang="ts">
-import { watch } from 'vue';
+import { watch,ref } from 'vue';
 import useCategoryStore from '@/store/modules/category';
-
+const showTable = ref<boolean>(false)
 const categoryStore = useCategoryStore();
 
 watch(
@@ -60,20 +82,36 @@ watch(
   }
 );
 
+const handleAdd = () => {
+  if(categoryStore.c3_id){
+    showTable.value = false
+  }
+    
+}
+const handleEdit = () => {
+  showTable.value = false
+}
 const getList = async () => {
   await categoryStore.getList();
 };
+
+const cancel = () => {
+  showTable.value = true;
+}
 </script>
 
 <style scoped lang="scss">
 body {
   margin: 0;
 }
+
 .example-showcase .el-loading-mask {
   z-index: 9;
 }
+
 .tags {
   display: flex;
+
   span {
     display: block;
     margin: 0 5px;
