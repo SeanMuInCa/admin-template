158369a578162d58848857ccc6af5b1cde850396
Sean <fengying1028@hotmail.com>
2024-04-03 22:47:06 -0600

    feat: store reset

--- src/views/product/Attitude.vue
+++ src/views/product/Attitude.vue
@@ -62,7 +62,7 @@
 </template>
 
 <script setup lang="ts">
-import { watch, reactive, ref } from 'vue';
+import { watch, reactive, ref,onBeforeUnmount } from 'vue';
 import useCategoryStore from '@/store/modules/category';
 import type { attr, attrValue } from '@/api/production/type';
 import { modifyAttr, deleteAttr } from '@/api/production/attribute';
@@ -185,6 +185,10 @@ const cancel = () => {
   // console.log(AttributeObj);
   // getList();
 };
+
+onBeforeUnmount(() => {
+  categoryStore.$reset();
+})
 </script>
 
 <style scoped lang="scss">
