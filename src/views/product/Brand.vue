54357edfd2ba27c2f00d78a5aec8e348dfb8b15e
Sean <fengying1028@hotmail.com>
2024-04-03 19:54:28 -0600

    feat: edit attributes

--- src/views/product/Brand.vue
+++ src/views/product/Brand.vue
@@ -1,46 +1,46 @@
 <template>
   <div>
     <Dialog :title="title" :show="show" :getStatus="getStatus" :flag="addFlag" v-if="show" :getData="getData" :rowData="rowData"></Dialog>
-  <el-card style="width: 100%; height: 100%">
-    <el-button type="primary" icon="Plus" @click="handleAdd">add a brand</el-button>
-    <!-- main data table -->
-    <el-table :data="tableData" style="width: 100%; margin: 20px 0" height="660" border>
-      <el-table-column fixed type="index" prop="index" label="No." width="100" align="center" />
-      <el-table-column prop="tmName" label="Brand name" />
-      <el-table-column prop="logoUrl" label="Brand Logo" style="height: '220px'">
-        <template #default="{ row }">
-          <div style="height: 180px">
-            <img :src="handleUrl(row.logoUrl)" alt="" style="width: 218px; height: 100%" />
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column fixed="right" label="Operations">
-        <template #default="{ row }">
-          <el-button type="warning" icon="Edit" @click="editBrand(row)"></el-button>
-          <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?" @confirm="confirm(row)">
-            <template #reference>
-              <el-button type="danger" icon="Delete"></el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card style="width: 100%; height: 100%">
+      <el-button type="primary" icon="Plus" @click="handleAdd">add a brand</el-button>
+      <!-- main data table -->
+      <el-table :data="tableData" style="width: 100%; margin: 20px 0" height="660" border>
+        <el-table-column fixed type="index" prop="index" label="No." width="100" align="center" />
+        <el-table-column prop="tmName" label="Brand name" />
+        <el-table-column prop="logoUrl" label="Brand Logo" style="height: '220px'">
+          <template #default="{ row }">
+            <div style="height: 180px">
+              <img :src="handleUrl(row.logoUrl)" alt="" style="width: 218px; height: 100%" />
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column fixed="right" label="Operations">
+          <template #default="{ row }">
+            <el-button type="warning" icon="Edit" @click="editBrand(row)"></el-button>
+            <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?" @confirm="confirm(row)">
+              <template #reference>
+                <el-button type="danger" icon="Delete"></el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <!-- pagination -->
-    <div class="demo-pagination-block">
-      <div class="demonstration"></div>
-      <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
-        :page-sizes="[3, 5, 7, 10]"
-        :background="true"
-        layout="prev, pager, next, jumper,->,sizes,total"
-        :total="total"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      />
-    </div>
-  </el-card>
+      <!-- pagination -->
+      <div class="demo-pagination-block">
+        <div class="demonstration"></div>
+        <el-pagination
+          v-model:current-page="currentPage"
+          v-model:page-size="pageSize"
+          :page-sizes="[3, 5, 7, 10]"
+          :background="true"
+          layout="prev, pager, next, jumper,->,sizes,total"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </el-card>
   </div>
 </template>
 
