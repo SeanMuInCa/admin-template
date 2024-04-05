d94b59ef3f439e0314700cac1fe0d4df76cbd56f
Sean <fengying1028@hotmail.com>
2024-04-04 23:10:10 -0600

    feat: init add sku page

---
+++ src/views/product/SKUForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card>
+    456
+    <el-button @click="cancel">click</el-button>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+const $emit = defineEmits(['setScene']);
+const cancel = () => {
+  $emit('setScene', 0);
+};
+</script>
+
+<style scoped></style>
