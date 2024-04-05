752d1b7a29dcdde0b137b2785eece01cca83d9bd
Sean <fengying1028@hotmail.com>
2024-04-04 23:09:11 -0600

    feat: get brand list

--- src/api/production/spu.ts
+++ src/api/production/spu.ts
@@ -2,6 +2,8 @@ import axiosRequest from '@/utils/request';
 import type { spuReturnType } from '@/api/production/type';
 enum API {
   GET_SPU_LIST = '/admin/product/',
+  GET_TRADEMARK_LIST = '/admin/product/baseTrademark/getTrademarkList',
 }
 
 export const getSPUList = (page: number, limit: number, category3Id: string | number) => axiosRequest.get<any, spuReturnType>(`${API.GET_SPU_LIST}/${page}/${limit}?category3Id=${category3Id}`);
+export const getTrademarkList = () => axiosRequest.get<any,any>(API.GET_TRADEMARK_LIST);
