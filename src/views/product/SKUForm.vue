f39d7dd06f45bdec10155a0e38699732bbdba684
Sean <fengying1028@hotmail.com>
2024-04-09 22:51:47 -0600

    feat: add sku

--- src/views/product/SKUForm.vue
+++ src/views/product/SKUForm.vue
@@ -34,7 +34,7 @@
       </el-form-item>
       <el-form-item label="logo name">
         <el-table border :data="spuImageList">
-          <el-table-column type="selection" width="80" align="center"></el-table-column>
+          <el-table-column type="selection" width="80" align="center" ></el-table-column>
           <el-table-column label="logo" prop="imgUrl">
             <template #default="{ row }">
               <img :src="row.imgUrl" style="width: 100px" />
@@ -72,12 +72,13 @@ const spuImageList = ref([]);
 
 const setAsDefault = (row) => {
   console.log(row);
+  
+  skuParams.value.skuDefaultImg = row.imgUrl;
 };
-
 const initSKUData = async (row) => {
   console.log(row);
 
-  const data = await getAttrList(categoryStore.c1_id, categoryStore.c2_id, categoryStore.c3_id);
+  const data:any = await getAttrList(categoryStore.c1_id, categoryStore.c2_id, categoryStore.c3_id);
   if (data.code == 200) {
     platformAttrList.value = data.data;
   }
