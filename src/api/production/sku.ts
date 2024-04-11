871268ad3f747e95e5ef641d604eda7ae1a3c3bf
Sean <fengying1028@hotmail.com>
2024-04-11 12:55:27 -0600

    feat: delete sku

--- src/api/production/sku.ts
+++ src/api/production/sku.ts
@@ -4,8 +4,12 @@ enum API {
   GET_SKU_LIST = '/admin/product/list/',
   ON_SALE = '/admin/product/onSale/',
   OFF_SALE = '/admin/product/cancelSale/',
+  GET_SKU_INFO = '/admin/product/getSkuInfo/',
+  DELETE_SKU = '/admin/product/deleteSku/',
 }
 
 export const getAllSKU = (page: number, limit: number) => axiosRequest.get<any, SKUListReturnType>(API.GET_SKU_LIST + `${page}/${limit}`);
 export const onSale = (id: number | string) => axiosRequest.get(API.ON_SALE + id);
 export const offSale = (id: number | string) => axiosRequest.get(API.OFF_SALE + id);
+export const getSKUInfo = (id: number | string) => axiosRequest.get(API.GET_SKU_INFO + id);
+export const deleteSku = (id: number | string) => axiosRequest.delete(API.DELETE_SKU + id);
--- src/views/product/SKU.vue
+++ src/views/product/SKU.vue
@@ -13,11 +13,14 @@
       <el-table-column prop="price" label="Price($)" width="200"></el-table-column>
       <el-table-column fixed="right" label="Operations" width="320">
         <template #default="{ row }">
-          <el-button v-if="!row.isSale" type="primary" icon="Top" size="small" title="OnSale SKU" @click="handleOnSale(row)"></el-button>
-          <el-button v-else type="info" icon="Bottom" size="small" title="OffSale SKU" @click="handleOnSale(row)"></el-button>
-          <el-button type="warning" icon="Edit" size="small" title="Edit SKU"></el-button>
-          <el-button type="info" icon="Warning" size="small" title="SKU Info"></el-button>
-          <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?">
+          <el-button v-if="!row.isSale" type="primary" icon="Top" size="small" title="OnSale SKU"
+            @click="handleOnSale(row)"></el-button>
+          <el-button v-else type="info" icon="Bottom" size="small" title="OffSale SKU"
+            @click="handleOnSale(row)"></el-button>
+          <el-button type="warning" icon="Edit" size="small" title="Coming soon" disabled></el-button>
+          <el-button type="info" icon="Warning" size="small" title="SKU Info" @click="handleInfo(row)"></el-button>
+          <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled"
+            icon-color="#626AEF" title="Are you sure to delete this?" @confirm="confirmDel(row)">
             <template #reference>
               <el-button type="danger" icon="Delete" size="small" title="Delete SKU"></el-button>
             </template>
@@ -28,22 +31,18 @@
     <!-- pagination -->
     <div class="demo-pagination-block">
       <div class="demonstration"></div>
-      <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
-        :page-sizes="[10, 20, 30]"
-        :background="true"
-        layout="prev, pager, next, jumper,->,sizes,total"
-        :total="total"
-        @size-change="handleSizeChange"
-        @current-change="getData"
-      />
+      <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 30]"
+        :background="true" layout="prev, pager, next, jumper,->,sizes,total" :total="total"
+        @size-change="handleSizeChange" @current-change="getData" />
     </div>
+    <el-drawer v-model="openDrawer" title="SKU Info Details" direction="rtl" size="50%">
+
+    </el-drawer>
   </el-card>
 </template>
 
 <script setup lang="ts">
-import { getAllSKU, onSale, offSale } from '@/api/production/sku';
+import { getAllSKU, onSale, offSale, getSKUInfo, deleteSku } from '@/api/production/sku';
 import { onMounted, ref } from 'vue';
 import { SKUType, SKUListReturnType, skuDataType } from '@/api/production/type';
 import { ElMessage } from 'element-plus';
@@ -51,6 +50,7 @@ const tableData = ref<SKUType[]>([]);
 const currentPage = ref<number>(1);
 const pageSize = ref<number>(10);
 const total = ref<number>(0);
+const openDrawer = ref(false);
 onMounted(() => {
   getData();
 });
@@ -74,18 +74,41 @@ const handleOnSale = async (row: skuDataType) => {
     if (data.code == 200) {
       ElMessage.info('offSale success');
     }
-  } else {
+  } else if (row.isSale === 0) {
     const data: any = await onSale(row.id);
     if (data.code == 200) {
       ElMessage.success('onSale success');
     }
   }
-  row.isSale = row.isSale === 0 ? 1 : 0;
+  getData(currentPage.value);
+};
+
+const handleInfo = async (row: skuDataType) => {
+  const data:any = await getSKUInfo(row.id);
+  if (data.code == 200) {
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+    openDrawer.value = true;
+  }
+}
+
+const confirmDel = async (row:skuDataType) => {
+  if(row.isSale === 1){
+    ElMessage.error('this SKU is onSale, can not delete!');
+    return;
+  }else{
+    const data:any = await deleteSku(row.id);
+  if (data.code == 200) {
+    ElMessage.success('deleted success!');
+    getData();
+  }
+  }
 };
 </script>
 
 <style scoped>
-.demo-pagination-block + .demo-pagination-block {
+.demo-pagination-block+.demo-pagination-block {
   margin-top: 10px;
 }
 
