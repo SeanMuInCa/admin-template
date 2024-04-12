02bc47af5057fcb479e89d80d48bc8d86d7a662d
Sean <fengying1028@hotmail.com>
2024-04-11 22:31:50 -0600

    feat: add user and delete user

--- src/api/acl/user.ts
+++ src/api/acl/user.ts
@@ -4,7 +4,9 @@ enum API {
   GET_ALL_USERS = '/admin/acl/user/',
   ADD_USER = '/admin/acl/user/save',
   UPDATE_USER = '/admin/acl/user/update',
+  MASS_DEL = '/admin/acl/user/batchRemove',
 }
 
 export const getAllUsers = (page: number, limit: number) => axiosRequest.get<any, UserListReturnType>(API.GET_ALL_USERS + `${page}/${limit}`);
 export const modifyUser = (data: userRecordsType) => (data.id ? axiosRequest.put<any, any>(API.UPDATE_USER, data) : axiosRequest.post<any, any>(API.ADD_USER, data));
+export const massDel = (data:any) => axiosRequest.delete<any,any>(API.MASS_DEL, data); 
--- src/views/acl/User.vue
+++ src/views/acl/User.vue
@@ -3,19 +3,19 @@
     <el-card>
       <el-form inline style="display: flex; justify-content: space-between; align-items: center">
         <el-form-item label="User Name" label-width="" style="margin: auto 10px">
-          <el-input placeholder="input a user's name to search" style="width: 300px"></el-input>
+          <el-input placeholder="input a user's name to search" style="width: 300px" v-model="nameToSearch"></el-input>
         </el-form-item>
         <el-form-item style="margin: auto 10px">
-          <el-button type="primary">Search</el-button>
-          <el-button>Reset</el-button>
+          <el-button type="primary" @click="searchName">Search</el-button>
+          <el-button @click="reset">Reset</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <el-card style="margin: 10px 0">
       <el-button type="primary" @click="openDrawer = true">Add One</el-button>
-      <el-button type="warning">Mass Delete</el-button>
-      <el-table border style="margin: 20px 0" :data="userData" :show-overflow-tooltip="true">
-        <el-table-column type="selection" width="50"></el-table-column>
+      <el-button type="warning" @click="massDelete" :disabled="!delList.length">Mass Delete</el-button>
+      <el-table border style="margin: 20px 0" :data="userData" :show-overflow-tooltip="true" ref="tableRef" @selection-change="selectRow">
+        <el-table-column type="selection" width="50" ></el-table-column>
         <el-table-column type="index" label="No." width="80" align="center"></el-table-column>
         <el-table-column label="User Id" width="80" prop="id" align="center"></el-table-column>
         <el-table-column label="User Name" width="140" prop="username"></el-table-column>
@@ -27,7 +27,8 @@
           <template #default="{ row }">
             <el-button type="primary" icon="User" size="small">Assign Role</el-button>
             <el-button type="warning" icon="Edit" size="small">Edit User</el-button>
-            <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?" @confirm="confirmDel(row)">
+            <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled"
+              icon-color="#626AEF" title="Are you sure to delete this?" @confirm="confirmDel(row)">
               <template #reference>
                 <el-button type="danger" icon="Delete" size="small">Delete</el-button>
               </template>
@@ -37,16 +38,9 @@
       </el-table>
       <div class="demo-pagination-block">
         <div class="demonstration"></div>
-        <el-pagination
-          v-model:current-page="currentPage"
-          v-model:page-size="pageSize"
-          :page-sizes="[5, 10, 15]"
-          :background="true"
-          layout="prev, pager, next, jumper,->,sizes,total"
-          :total="total"
-          @size-change="handleSizeChange"
-          @current-change="getData"
-        />
+        <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[5, 10, 15]"
+          :background="true" layout="prev, pager, next, jumper,->,sizes,total" :total="total"
+          @size-change="handleSizeChange" @current-change="getData" />
       </div>
       <el-drawer v-model="openDrawer" title="Add a user" direction="rtl" size="30%">
         <template #default>
@@ -83,7 +77,7 @@
 </template>
 
 <script setup lang="ts">
-import { getAllUsers, modifyUser } from '@/api/acl/user';
+import { getAllUsers, modifyUser,massDel } from '@/api/acl/user';
 import { ref, onMounted } from 'vue';
 import type { userRecordsType, UserListReturnType } from '@/api/acl/type';
 import { ElMessage } from 'element-plus';
@@ -91,8 +85,17 @@ const currentPage = ref(1);
 const pageSize = ref(5);
 const total = ref(0);
 const userData = ref<userRecordsType[]>([]);
-const userParams = ref<userRecordsType>({});
+const userParams = ref<userRecordsType>({
+
+  name: '',
+  username: '',
+  password: '',
+
+});
 const openDrawer = ref<boolean>(false);
+const nameToSearch = ref<string>('');
+const tableRef = ref<any>();
+const delList = ref<userRecordsType[]>([])
 const getData = async (pager = 1) => {
   currentPage.value = pager;
   const data: UserListReturnType = await getAllUsers(currentPage.value, pageSize.value);
@@ -128,16 +131,43 @@ const confirmAdd = async () => {
     ElMessage.error('something went wrong');
   }
 };
+
+const searchName = () => {
+  if(nameToSearch.value.trim()){
+    userData.value = userData.value.filter(item => item.name.includes(nameToSearch.value));
+  }else getData();
+}
+const reset = () => {
+  nameToSearch.value = '';
+  getData();
+}
+
+const massDelete = async() => {
+  let idList:number[] = delList.value.map(item => item.id);
+  if(idList.length > 0){
+    const data = await massDel({idList});
+    if(data.code == 200){
+      ElMessage.success('deleted!')
+    }else{
+      ElMessage.error('something went wrong');
+    }
+  }
+  getData();
+}
+const selectRow = (data:userRecordsType[]) => {
+  delList.value = data;
+}
 </script>
 
 <style scoped>
-.demo-pagination-block + .demo-pagination-block {
+.demo-pagination-block+.demo-pagination-block {
   margin-top: 10px;
 }
 
 .demo-pagination-block .demonstration {
   margin-bottom: 16px;
 }
+
 .el-row {
   margin-bottom: 20px;
 }
