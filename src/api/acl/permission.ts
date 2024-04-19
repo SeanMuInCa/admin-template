b6efb16d2dfa8787d22de14319688de004b06981
Sean <fengying1028@hotmail.com>
2024-04-18 20:35:53 -0600

    feat: permission page init

--- src/api/acl/permission.ts
+++ src/api/acl/permission.ts
@@ -1,9 +1,7 @@
 import axiosRequest from '@/utils/request';
 
-enum API{
-    GET_ALL_MENU = '/admin/acl/permission',
+enum API {
+  GET_ALL_MENU = '/admin/acl/permission',
 }
 
-export const getAllMenu = () => axiosRequest.get<any,any>(API.GET_ALL_MENU);
-
-
+export const getAllMenu = () => axiosRequest.get<any, any>(API.GET_ALL_MENU);
--- src/views/acl/Permission.vue
+++ src/views/acl/Permission.vue
@@ -6,17 +6,16 @@
       <el-table-column label="Last updated" prop="updateTime"></el-table-column>
       <el-table-column label="Operation">
         <template #default="{ row }">
-            <el-button type="primary" icon="Plus" size="small" >Add</el-button>
-            <el-button type="warning" icon="Edit" size="small" :disabled="row.id === 1">Edit</el-button>
-            <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?" >
-              <template #reference>
-                <el-button type="danger" icon="Delete" size="small" :disabled="row.id === 1">Delete</el-button>
-              </template>
-            </el-popconfirm>
-          </template>
+          <el-button type="primary" icon="Plus" size="small">Add</el-button>
+          <el-button type="warning" icon="Edit" size="small" :disabled="row.id === 1">Edit</el-button>
+          <el-popconfirm width="220" confirm-button-text="OK" cancel-button-text="No, Thanks" icon="InfoFilled" icon-color="#626AEF" title="Are you sure to delete this?">
+            <template #reference>
+              <el-button type="danger" icon="Delete" size="small" :disabled="row.id === 1">Delete</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
       </el-table-column>
     </el-table>
-
   </div>
 </template>
 
@@ -26,7 +25,7 @@ import { getAllMenu } from '@/api/acl/permission';
 const allMenu = ref([]);
 onMounted(() => {
   getData();
-})
+});
 
 const getData = async () => {
   const data = await getAllMenu();
@@ -34,7 +33,7 @@ const getData = async () => {
     console.log(data);
     allMenu.value = data.data;
   }
-}
+};
 </script>
 
 <style scoped></style>
