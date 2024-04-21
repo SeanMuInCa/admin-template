bd22f9bc3031630aa8bc6e31883636134456c83f
Sean <fengying1028@hotmail.com>
2024-04-21 13:06:42 -0600

    feat: init screen

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>screen</div>
+  <div class="container">
+    <div class="screen">
+      <div class="top">123</div>
+      <div class="main">
+        <div class="left"></div>
+        <div class="center"></div>
+        <div class="right"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container{
+  width: 100vw;
+  height: 100vh;
+  background: url('./images/bg.png') no-repeat;
+  background-size: cover;
+  .screen{
+    width: 1920px;
+    height: 1080px;
+    background-color: red;
+  }
+}
+</style>
