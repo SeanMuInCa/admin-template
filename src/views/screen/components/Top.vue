066ff8c3858ac8ed61d54890e511ee876eded6db
Sean <fengying1028@hotmail.com>
2024-04-21 20:49:21 -0600

    feat: big screen

---
+++ src/views/screen/components/Top.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="top">
+        <div class="left">
+            <span class="lbtn">Home Page</span>
+        </div>
+        <div class="center">1</div>
+        <div class="right">
+            <span class="rbtn">Report</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.top{
+    display: flex;
+    width: 100%;
+    height: 40px;
+    .left{
+        flex: 1;
+        background: url('../images/dataScreen-header-left-bg.png') no-repeat;
+        background-size: cover;
+        .lbtn{
+            width: 135px;
+            height: 40px;
+            position: absolute;
+            left: 260px;
+            background: url('../images/dataScreen-header-btn-bg-l.png') no-repeat;
+            background-size: cover;
+            text-align: center;
+            line-height: 40px;
+            color: #29fcff;
+            cursor: pointer;
+        }
+
+    }
+    .center{
+        height: 74px;
+        flex:3;
+        background: url('../images/dataScreen-header-center-bg.png') no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+    .right{
+        flex: 1;
+        background: url('../images/dataScreen-header-right-bg.png') no-repeat;
+        background-size: cover;
+        .rbtn{
+            width: 135px;
+            height: 40px;
+            position: absolute;
+            right: 260px;
+            background: url('../images/dataScreen-header-btn-bg-r.png') no-repeat;
+            background-size: cover;
+            text-align: center;
+            line-height: 40px;
+            color: #29fcff;
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="container">
-    <div class="screen">
-      <div class="top">123</div>
+    <div class="screen" ref="screenRef">
+      <div class="top">
+        <Top></Top>
+      </div>
       <div class="main">
-        <div class="left"></div>
-        <div class="center"></div>
-        <div class="right"></div>
+        <div class="left">1</div>
+        <div class="center">2</div>
+        <div class="right">3</div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import Top from './components/Top.vue';
+
+const screenRef = ref();
+onMounted(() => {
+  screenRef.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+})
+
+//大屏缩放比例函数
+const getScale = (w=1920, h=1080) => {
+  let ww = window.innerWidth / w;
+  let wh = window.innerHeight / h;
+  return ww < wh ? ww : wh;
+}
+</script>
 
 <style scoped lang="scss">
 .container {
@@ -20,9 +37,29 @@
   background: url('./images/bg.png') no-repeat;
   background-size: cover;
   .screen {
+    position: fixed;
+    left: 50%;
+    top: 50%;
     width: 1920px;
     height: 1080px;
-    background-color: red;
+    // background-color: red;
+    transform-origin: left top;
+    .top{
+      width: 100%;
+      height: 40px;
+    }
+    .main{
+      display: flex;
+      .left {
+        flex: 1;
+      }
+      .center{
+        flex: 3;
+      }
+      .right{
+        flex: 1;
+      }
+    }
   }
 }
 </style>
