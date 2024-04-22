91cb50c9f12fc1b0feb59018f225f1daf8f89b92
Sean <fengying1028@hotmail.com>
2024-04-21 21:21:43 -0600

    feat: use moment lib

--- src/views/screen/components/Top.vue
+++ src/views/screen/components/Top.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="top">
     <div class="left">
-      <span class="lbtn">Home Page</span>
+      <span class="lbtn" @click="goHome">Home Page</span>
     </div>
-    <div class="center">1</div>
+    <div class="center">
+        Data Screen for Duesytech System
+    </div>
     <div class="right">
-      <span class="rbtn">Report</span>
+      <div class="rbtn">Report</div>
+      <div></div>
+      <div style="margin-right: 40px;">{{ time }}</div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { router } from '@/router/index';
+import moment from 'moment';
+import { ref } from 'vue';
+const time = ref('')
+setInterval(() => {
+    time.value = moment().format('YYYY-MM-DD hh-mm-ss');
+},1000)
+
+const goHome = () => {
+    router.push('/home');
+}
+
+</script>
 
 <style scoped lang="scss">
 .top {
@@ -36,15 +53,24 @@
   }
   .center {
     height: 74px;
+    color: #29fcff;
+    line-height: 74px;
+    text-align: center;
+    font-size: 46px;
     flex: 3;
     background: url('../images/dataScreen-header-center-bg.png') no-repeat;
     background-position: center;
-    background-size: cover;
+    background-size: 100% 100%;
   }
   .right {
     flex: 1;
     background: url('../images/dataScreen-header-right-bg.png') no-repeat;
-    background-size: cover;
+    background-size: 100% 100%;
+    color: #29fcff;
+    line-height: 40px;
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
     .rbtn {
       width: 135px;
       height: 40px;
@@ -52,9 +78,7 @@
       right: 260px;
       background: url('../images/dataScreen-header-btn-bg-r.png') no-repeat;
       background-size: cover;
-      text-align: center;
       line-height: 40px;
-      color: #29fcff;
       cursor: pointer;
     }
   }
