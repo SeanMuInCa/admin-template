354f305269e1af17c4b8fb52d8cd3c30e36e7fee
Sean <fengying1028@hotmail.com>
2024-04-23 13:11:45 -0600

    feat: 3rd chart

--- src/views/screen/components/LeftBottom.vue
+++ src/views/screen/components/LeftBottom.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="box">
-        <div class="top">
-            <p class="static">Rate</p>
-            <p class="title"></p>
-        </div>
-        <div class="chart" ref="chart1"></div>
+  <div class="box">
+    <div class="top">
+      <p class="static">Rate</p>
+      <p class="title"></p>
     </div>
+    <div class="chart" ref="chart1"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,89 +16,89 @@ onMounted(() => {
   const myChart = echarts.init(chart1.value);
   myChart.setOption({
     tooltip: {
-    trigger: 'item'
-  },
-  legend: {
-    orient:'vertical',
-    right: '10',
-    top:50,
-    textStyle:{
-        color:'#fff',
-        fontSize:'18'
-    }
-  },
-  series: [
-    {
-      name: 'Access From',
-      type: 'pie',
-      radius: ['30%', '70%'],
-      avoidLabelOverlap: false,
-      center:['30%', '50%'],
-      itemStyle: {
-        borderRadius: 10,
-        borderColor: '#fff',
-        borderWidth: 2
+      trigger: 'item',
+    },
+    legend: {
+      orient: 'vertical',
+      right: '10',
+      top: 50,
+      textStyle: {
+        color: '#fff',
+        fontSize: '18',
       },
-      label: {
-        show: true,
-        position: 'inside',
-        color:'#fff',
-        fontWeight:'bold',
-        padding: 4,
-        borderColor:'red'
-      },
-      emphasis: {
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['30%', '70%'],
+        avoidLabelOverlap: false,
+        center: ['30%', '50%'],
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
         label: {
           show: true,
-          fontSize: 40,
-          fontWeight: 'bold'
-        }
+          position: 'inside',
+          color: '#fff',
+          fontWeight: 'bold',
+          padding: 4,
+          borderColor: 'red',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold',
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: 'teen' },
+          { value: 735, name: '20~30' },
+          { value: 580, name: '30~40' },
+          { value: 484, name: '40~50' },
+          { value: 300, name: 'elder' },
+        ],
       },
-      labelLine: {
-        show: false
-      },
-      data: [
-        { value: 1048, name: 'teen' },
-        { value: 735, name: '20~30' },
-        { value: 580, name: '30~40' },
-        { value: 484, name: '40~50' },
-        { value: 300, name: 'elder' }
-      ]
-    }
-  ],
-  grid:{
-    top:0,
-    left:0,
-    right:0,
-    bottom:0
-  }
-  })
-})
+    ],
+    grid: {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+  });
+});
 </script>
 
 <style scoped lang="scss">
 .box {
-    background-color: aqua;
-    background: url('../images/dataScreen-main-lb.png') no-repeat;
-    background-size: 100% 100%;
-    height: 262px;
+  background-color: aqua;
+  background: url('../images/dataScreen-main-lb.png') no-repeat;
+  background-size: 100% 100%;
+  height: 262px;
 
-    .top {
-        color: white;
-        margin: 5px 20px;
+  .top {
+    color: white;
+    margin: 5px 20px;
 
-        .title {
-            background: url('../images/dataScreen-title.png') no-repeat;
-            background-size: contain;
-            height: 7px;
-            width: 68px;
-            margin-top: 10px;
-        }
+    .title {
+      background: url('../images/dataScreen-title.png') no-repeat;
+      background-size: contain;
+      height: 7px;
+      width: 68px;
+      margin-top: 10px;
     }
-    .chart{
-        width: 100%;
-        height:260px;
-        padding: 10px;
-    }
+  }
+  .chart {
+    width: 100%;
+    height: 260px;
+    padding: 10px;
+  }
 }
 </style>
